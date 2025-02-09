f8294016e7548782596143b9f2f83aed3ad935a8
Martin Donadieu <martindonadieu@gmail.com>
2025-02-09 05:01:32 +0000

    fix: remove old files

--- ios/Plugin/NativeGeocoderPlugin.m
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-#import <Capacitor/Capacitor.h>
-
-// Define the plugin using the CAP_PLUGIN Macro, and
-// each method the plugin supports using the CAP_PLUGIN_METHOD macro.
-CAP_PLUGIN(NativeGeocoderPlugin, "NativeGeocoder",
-           CAP_PLUGIN_METHOD(reverseGeocode, CAPPluginReturnPromise);
-           CAP_PLUGIN_METHOD(forwardGeocode, CAPPluginReturnPromise);
-)
