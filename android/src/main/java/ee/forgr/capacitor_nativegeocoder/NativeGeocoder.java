5cfbd456b0db55aec0cc1c39f5ec3b7eae1bd7b3
Martin Donadieu <martindonadieu@gmail.com>
2025-12-21 11:27:17 +0000

    refactor: streamline locale handling in Geocoder creation by removing Build version checks

--- android/src/main/java/ee/forgr/capacitor_nativegeocoder/NativeGeocoder.java
+++ android/src/main/java/ee/forgr/capacitor_nativegeocoder/NativeGeocoder.java
@@ -5,7 +5,6 @@ import android.location.Address;
 import android.location.Geocoder;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.os.Build;
 import com.getcapacitor.JSObject;
 import com.getcapacitor.PluginCall;
 import java.util.List;
@@ -216,15 +215,7 @@ public class NativeGeocoder {
      */
     private Geocoder createGeocoderWithOptions(NativeGeocoderOptions geocoderOptions) {
         if (geocoderOptions.defaultLocale != null && !geocoderOptions.defaultLocale.isEmpty()) {
-            Locale locale;
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                locale = Locale.forLanguageTag(geocoderOptions.defaultLocale);
-            } else {
-                String[] parts = geocoderOptions.defaultLocale.split("[-_]", -1);
-                if (parts.length == 1) locale = new Locale(parts[0]);
-                else if (parts.length == 2 || (parts.length == 3 && parts[2].startsWith("#"))) locale = new Locale(parts[0], parts[1]);
-                else locale = new Locale(parts[0], parts[1], parts[2]);
-            }
+            Locale locale = Locale.forLanguageTag(geocoderOptions.defaultLocale);
             geocoder = new Geocoder(context.getApplicationContext(), locale);
         } else {
             if (geocoderOptions.useLocale) {
